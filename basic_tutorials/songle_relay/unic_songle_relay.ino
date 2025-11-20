e06ce6d74e66d000351c3f4dc1525c92cc95a9cc
Wesley-SR <wesley.sr@outlook.com>
2025-11-20 08:46:47 -0300

    First Commit

---
+++ basic_tutorials/songle_relay/unic_songle_relay.ino
@@ -0,0 +1,24 @@
+// Programa de teste do modulo rele
+// Autor : Arduino e Cia.
+// Source: https://arduinoecia.com.br/ligando-uma-lampada-com-modulo-rele-arduino/
+//
+// Ligar o pino S do rele no pino 7 do Arduino
+// Ligar o pino + do rele no pino 5V do Arduino
+// Ligar o pino - do rele no pino GND do Arduino
+
+//define a saida a ser utilizada para o acionamento do rele
+int sinalparaorele = 7; 
+
+void setup()
+{
+  pinMode(sinalparaorele, OUTPUT); //Define o pino como saida
+}
+
+void loop()
+
+{
+  digitalWrite(sinalparaorele, HIGH); //Aciona o rele
+  delay(5000); //Aguarda 5 segundos
+  digitalWrite(sinalparaorele, LOW); //Desliga o rele
+  delay(5000); //Aguarda 5 segundos e reinicia o processo
+}
